8581e06f20b771c18a7788c944f2d191fb92cba7
davidalmarinho <davidalmarinho@gmail.com>
2021-07-30 14:29:41 +0100

    Setting up the Window

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import jade.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create window
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/jade/Window.java
@@ -0,0 +1,108 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private int width, height;
+    private final String title;
+    private static Window window;
+    private long glfwWindow; // Vai agir como se fosse um ponteiro
+
+    private Window() {
+        this.width  = 1920;
+        this.height = 1080;
+        this.title  = "Mario";
+    }
+
+    public static Window get() {
+        if (window == null) {
+            window = new Window();
+        }
+
+        return window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Callback erro --se houver erros, vamos mostrá-los aqui
+        GLFWErrorCallback.createPrint(System.err).set();
+        /* Existem três tipos de comandos System no Java:
+         * -> System.err => Output padrão dos erros do sistema (Daí usarmos este nesta situação,
+         * pois "queremos" mostrar erros)
+         * -> System.in => Input padrão do sistema, daí, quando criamos um Scanner para ler dados do usuário
+         * fazemos:
+         *         Scanner scanner = new Scanner(System.in);
+         * -> System.out => Faz output de dados (não é muito certo para aqui por termos o System.err que é
+         * mais específico para este caso, mas se não usaríamos o System.out).
+         */
+
+        // Inicializar o GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Error: GLFW couldn't be Initialized.");
+        }
+
+        // Configurar o GLFW (resizable window, close operation...)
+        /* Configuramos primeiro e depois criamos a janela, pois o GLFW vai usar isto par criar
+        * a janela
+        */
+        glfwDefaultWindowHints(); // Colocar tudo default primeiro
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // Ainda não a queremos visível
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Criar a janela
+        /* O primeiro NULL serve para selecionar o monitor (NULL vai indicar o monitor primário)
+         * O segundo NULL serve para ligar shared objects. Mas não é multiplataforma, por isso
+         * vamos fazê-lo nós.
+         */
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        // Verificar se a janela foi criada
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Error: GLFW windows couldn't be initialized");
+        }
+
+        // Contexto do OpenGL
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Ligar o v-sync (usar quantos hz o monitor tiver para dar refresh aos frames)
+        glfwSwapInterval(1);
+
+        // Agora vamos tornar a janela visível
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Carregar os eventos (teclado...)
+            glfwPollEvents();
+
+            // Limpar a frame com uma cor
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f); /* Especifica a cor que o glClear vai usar para
+            limpar a color buffers */
+            glClear(GL_COLOR_BUFFER_BIT); /* Contar para o OpenGL como limpar a frame (Indicates the buffers
+            currently enabled for color writing).*/
+
+            glfwSwapBuffers(glfwWindow); /* Faz o mesmo que o Bufferstrategy, aquela parte de já termos uma
+            imagem pronta para mostrar antes de apagarmos a outra. */
+        }
+    }
+}
