8ee4b59b8e6ee242aafd7455a902ce6f394eeedb
davidalmarinho <davidalmarinho@gmail.com>
2021-07-31 14:01:10 +0100

    New assets: MouseListener KeyListener We have a fade to black animation to test keyboard input

---
+++ src/main/java/jade/MouseListener.java
@@ -0,0 +1,130 @@
+package jade;
+
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double xPos, yPos, lastXPos, lastYPos;
+    private double scrollX, scrollY;
+    private final boolean[] mouseButtons = new boolean[3];
+    private boolean dragging;
+
+    private MouseListener() {
+        // Definir os valores
+        this.xPos     = 0.0;
+        this.yPos     = 0.0;
+        this.lastXPos = 0.0;
+        this.lastYPos = 0.0;
+        this.scrollX  = 0.0;
+        this.scrollY  = 0.0;
+        this.dragging = false;
+    }
+
+    /**
+     * Para receber as coordenadas do rato
+     * Vamos usar get().lastX, get().lastY()... Para futuramente pudermos chamar
+     * MouseListener.mousePositionCallback() ao invés de MouseListener.get().mousePositionCallback(),
+     * pois está tudo corretamente instanciado/estático.
+     * @param xPos (Posição) x atual do rato
+     * @param yPos (Posição) y atual do rato
+     */
+    public static void mousePosCallback(long window, double xPos, double yPos) {
+        // Copiar os valores antigos para não os perdermos -- neste caso, coordenadas da última posição do rato
+        get().lastXPos = get().xPos;
+        get().lastYPos = get().yPos;
+
+        // Receber os valores atuais -- coordenadas atuais
+        get().xPos = xPos;
+        get().yPos = yPos;
+
+        // Verificar se estamos a arrastar algum elemento
+        for (int indexButton = 0; indexButton < get().mouseButtons.length; indexButton++) {
+            // Se estivermos a clicar em algum botão do rato, então dragging tem que ser true
+            if (get().mouseButtons[indexButton]) {
+                get().dragging = true;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Para o jogo reconhecer keycodes
+     * @param window Ponteiro da janela
+     * @param button Botão pressionado
+     * @param action Verificar se botão foi pressionado
+     * @param mods Para keybindings
+     */
+    public static void mouseButtonCallback(long window, int button, int action, int mods) {
+        if (action == GLFW_PRESS) {
+            // Verificar se "temos as teclas todas"
+            if (button < get().mouseButtons.length) {
+                get().mouseButtons[button] = true;
+            }
+        } else if (action == GLFW_RELEASE) {
+            if (button < get().mouseButtons.length) {
+                get().mouseButtons[button] = false;
+
+                // Como não clicamos em nenhum botão, sabemos que também não há dragging
+                get().dragging = false;
+            }
+        }
+    }
+
+    public static void mouseScrollCallback(long window, double scrollX, double scrollY) {
+        get().scrollX = scrollX;
+        get().scrollY = scrollY;
+    }
+
+    public static void endFrame() {
+        // Guardar e dar reset a valores
+        get().lastXPos = get().xPos;
+        get().lastYPos = get().yPos;
+        get().scrollX  = 0.0;
+        get().scrollY  = 0.0;
+    }
+
+    public static float getXPos() {
+        return (float) get().xPos;
+    }
+
+    public static float getYPos() {
+        return (float) get().yPos;
+    }
+
+    public static float getDeltaX() {
+        return (float) (get().lastXPos - get().xPos);
+    }
+
+    public static float getDeltaY() {
+        return (float) (get().lastYPos - get().yPos);
+    }
+
+    public static float getScrollX() {
+        return (float) get().scrollX;
+    }
+
+    public static float getScrollY() {
+        return (float) get().scrollY;
+    }
+
+    public static boolean isDragging() {
+        return get().dragging;
+    }
+
+    public static boolean mouseButtonDown(int button) {
+        if (button < get().mouseButtons.length) {
+            return get().mouseButtons[button];
+        }
+
+        return false;
+    }
+
+    public static MouseListener get() {
+        if (instance == null) {
+            instance = new MouseListener();
+        }
+
+        return instance;
+    }
+}
--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -4,6 +4,7 @@ import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.NULL;
@@ -13,11 +14,17 @@ public class Window {
     private final String title;
     private static Window window;
     private long glfwWindow; // Vai agir como se fosse um ponteiro
+    private boolean fadingToBlack = false;
+    private float r, g, b, a;
 
     private Window() {
         this.width  = 1920;
         this.height = 1080;
         this.title  = "Mario";
+        this.r = 1.0f;
+        this.g = 1.0f;
+        this.b = 1.0f;
+        this.a = 1.0f;
     }
 
     public static Window get() {
@@ -33,6 +40,18 @@ public class Window {
 
         init();
         loop();
+
+        /* Esta parte não é estritamente necessária, porque o, sistema operativo,
+         * já costuma limpar a memória, mas podemos ser nós a limpar o nosso lixo :)
+         */
+
+        // Libertar a memória
+        glfwFreeCallbacks(glfwWindow); // Limpar os erros --Dá reset aos erros da janela
+        glfwDestroyWindow(glfwWindow); // Destruir a janela
+
+        // Terminar todas as execuções da janela
+        glfwTerminate(); // Termina a biblioteca glfwWindowHint
+        glfwSetErrorCallback(null).free(); // Libertar o error callback
     }
 
     public void init() {
@@ -73,6 +92,14 @@ public class Window {
             throw new IllegalStateException("Error: GLFW windows couldn't be initialized");
         }
 
+        // Configurar os callbacks do rato
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+
+        // Configurar callbacks do teclado
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
         // Contexto do OpenGL
         glfwMakeContextCurrent(glfwWindow);
 
@@ -92,12 +119,30 @@ public class Window {
 
     public void loop() {
         while (!glfwWindowShouldClose(glfwWindow)) {
+            KeyListener.updateLastKeys();
+
             // Carregar os eventos (teclado...)
             glfwPollEvents();
 
+            // System.out.println("Position x: " + MouseListener.getXPos());
+            // System.out.println("Position y " + MouseListener.getYPos());
+            // if (KeyListener.isKeyUp(GLFW_KEY_SPACE)) {
+            //     System.out.println("Hello World!");
+            // }
+
             // Limpar a frame com uma cor
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f); /* Especifica a cor que o glClear vai usar para
+            glClearColor(r, g, b, a); /* Especifica a cor que o glClear vai usar para
             limpar a color buffers */
+
+            if (KeyListener.isKeyDown(GLFW_KEY_SPACE)) fadingToBlack = true;
+
+
+            if (fadingToBlack) {
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+            }
+
             glClear(GL_COLOR_BUFFER_BIT); /* Contar para o OpenGL como limpar a frame (Indicates the buffers
             currently enabled for color writing).*/
 
