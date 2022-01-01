b6373c54d17fdeff1f799c456dbc7fad7f6bde68
David Marinho <davidalmarinho@gmail.com>
2022-01-01 11:18:00 +0000

    New assets:
    Added methods to check if 2 floats are equal and if 2 vectors of float type are equal depending on an error margin

--- src/main/java/jade/utils/JMath.java
+++ src/main/java/jade/utils/JMath.java
@@ -31,4 +31,50 @@ public class JMath {
         yPrime += objectCenter.y;
         vertex.set(xPrime, yPrime);
     }
+
+    /**
+     * Checks if 2 floats are equal depending on an error margin
+     *
+     * @param f1 first float
+     * @param f2 second float
+     * @param epsilon error margin
+     * @return true if they are equal depending on this error margin
+     */
+    public static boolean compare(float f1, float f2, float epsilon) {
+        return Math.abs(f1 - f2) <= epsilon * Math.max(1.0f, Math.max(Math.abs(f1), Math.abs(f2)));
+    }
+
+    /**
+     * Checks if 2 floats are equal depending on an error margin
+     *
+     * @param f1 first float
+     * @param f2 second float
+     * @return true if they are equal with a margin of error of Float.MIN_VALUE
+     */
+    public static boolean compare(float f1, float f2) {
+        return compare(f1, f2, Float.MIN_VALUE);
+    }
+
+    /**
+     * Checks if 2 vectors type float are equal depending on an error margin
+     *
+     * @param vec1 first type float vector
+     * @param vec2 second type float vector
+     * @param epsilon required error margin
+     * @return true if the 2 vectors are equal depending on the required margin of error
+     */
+    public static boolean compare(Vector2f vec1, Vector2f vec2, float epsilon) {
+        return compare(vec1.x, vec2.x, epsilon) && compare(vec1.y, vec2.y, epsilon);
+    }
+
+    /**
+     * Checks if 2 vectors type float are equal depending on an error margin
+     *
+     * @param vec1 first type float vector
+     * @param vec2 second type float vector
+     * @return true if the 2 vectors are equal depending on a margin of error of Float.MIN_VALUE
+     */
+    public static boolean compare(Vector2f vec1, Vector2f vec2) {
+        return compare(vec1, vec2, Float.MIN_VALUE);
+    }
 }
