e5cfa7bb19867f357f03668340f4ce403fb3edf1
davidalmarinho <davidalmarinho@gmail.com>
2022-04-07 23:10:16 +0100

    Pipes:
    Add hotkeys to move the game objects and change its z index.

--- src/main/java/jade/editor/components/KeyControls.java
+++ src/main/java/jade/editor/components/KeyControls.java
@@ -3,7 +3,6 @@ package jade.editor.components;
 import gameobjects.GameObject;
 import gameobjects.components.Component;
 import jade.SiriusTheFox;
-import jade.Window;
 import jade.animations.StateMachine;
 import jade.editor.PropertiesWindow;
 import jade.input.KeyListener;
@@ -15,12 +14,17 @@ import java.util.List;
 import static org.lwjgl.glfw.GLFW.*;
 
 public class KeyControls extends Component {
+    private float debounceTime = 0.2f;
+    private float debounce = 0.0f;
 
     @Override
     public void editorUpdate(float dt) {
+        debounce -= dt;
+
         PropertiesWindow propertiesWindow = SiriusTheFox.getImGuiLayer().getPropertiesWindow();
         GameObject activeGameObject = propertiesWindow.getActiveGameObject();
         List<GameObject> activeGameObjectList = propertiesWindow.getActiveGameObjectList();
+        float multiplayer = KeyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT) ? 0.1f : 1.0f;
 
         if (KeyListener.isBindPressed(GLFW_KEY_LEFT_CONTROL, GLFW_KEY_D) && activeGameObject != null) {
             GameObject newObj = activeGameObject.copy();
@@ -52,6 +56,36 @@ public class KeyControls extends Component {
                 go.destroy();
             }
             propertiesWindow.clearSelected();
+        } else if (KeyListener.isKeyDown(GLFW_KEY_PAGE_UP) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.zIndex++;
+            }
+        } else if (KeyListener.isKeyDown(GLFW_KEY_PAGE_DOWN) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.zIndex--;
+            }
+        } else if (KeyListener.isKeyDown(GLFW_KEY_UP) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.position.y += Settings.GRID_HEIGHT * multiplayer;
+            }
+        } else if (KeyListener.isKeyDown(GLFW_KEY_LEFT) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.position.x -= Settings.GRID_WIDTH * multiplayer;
+            }
+        } else if (KeyListener.isKeyDown(GLFW_KEY_RIGHT) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.position.x += Settings.GRID_HEIGHT * multiplayer;
+            }
+        } else if (KeyListener.isKeyDown(GLFW_KEY_DOWN) && debounce < 0) {
+            debounce = debounceTime;
+            for (GameObject go : activeGameObjectList) {
+                go.transform.position.y -= Settings.GRID_HEIGHT * multiplayer;
+            }
         }
     }
 }
