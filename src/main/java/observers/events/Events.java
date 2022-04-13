6b091d8b8eab712b34b5704158bd8fc2476e267c
David Marinho <davidalmarinho@gmail.com>
2022-04-13 12:49:33 +0100

    DistributingGame:
    Added export setting to the engine. For now, it only turns off ImGui and the debug rendering, doesn't export the game.

--- src/main/java/observers/events/Events.java
+++ src/main/java/observers/events/Events.java
@@ -1,10 +1,11 @@
 package observers.events;
 
-public enum EEventType {
+public enum Events {
     GAME_ENGINE_START_PLAY,
     GAME_ENGINE_STOP_PLAY,
     SAVE_LEVEL,
     LOAD_LEVEL,
+    EXPORT_GAME,
     // For the users to create their own events
     USER_EVENT,
 }
